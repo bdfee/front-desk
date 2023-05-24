f9efeb0edb39467b98a6b7f666aeb063dc7a6d68
Ben Durfee <ben.p.durfee@gmail.com>
2023-05-24 16:35:11 -0400

    add appointment routes

--- server/controllers/appointments.ts
+++ server/controllers/appointments.ts
@@ -9,7 +9,7 @@ router.get("/", async (_req, res) => {
     res.json(appointments);
   } catch (error) {
     console.log("Error getting appointments:", error);
-    res.status(500).json({ error: "Error getting appointments" });
+    res.status(400).json({ error: "Error getting appointments" });
   }
 });
 
@@ -19,7 +19,42 @@ router.post("/", async (req, res) => {
     res.json(appointment);
   } catch (error) {
     console.log("Error creating appointments", error);
-    res.status(500).json({ error: "Error getting appointments" });
+    res.status(400).json({ error: "Error getting appointments" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const appointment = await Appointment.findByPk(req.params.id);
+    res.json(appointment);
+  } catch (error) {
+    console.log("Error getting appointment: ", error);
+    res.status(400).json({ error: "Error getting appointment: " + error });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const appointment = await Appointment.findByPk(req.params.id);
+    const updatedAppointment = await appointment?.update({ ...appointment, ...req.body });
+    res.json(updatedAppointment);
+  } catch (error) {
+    console.log("Error updating appointment: ", error);
+    res.status(400).json({ error: "Error updating appointment: " + error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Appointment.destroy({
+      where: {
+        appointmentId: req.params.id,
+      },
+    });
+    res.status(204).end();
+  } catch (error) {
+    console.log("Error deleting appointment: ", error);
+    res.status(400).json({ error: "Error deleting appointment: " + error });
   }
 });
 
