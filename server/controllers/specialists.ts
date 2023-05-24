e6e8853deb259d64424b81286f3101b3b2af946a
Ben Durfee <ben.p.durfee@gmail.com>
2023-05-24 15:30:27 -0400

    add specialist routes, typescript no unsafe arg  disabled

--- server/controllers/specialists.ts
+++ server/controllers/specialists.ts
@@ -10,7 +10,7 @@ router.get("/", async (_req, res) => {
     res.json(specialists);
   } catch (error) {
     console.log("Error getting specialists: ", error);
-    res.status(500).json({ error: "Error getting specialists" });
+    res.status(500).json({ error: "Error getting specialists: " + error });
   }
 });
 
@@ -20,7 +20,30 @@ router.post("/", async (req, res) => {
     res.json(specialist);
   } catch (error) {
     console.log("Error creating specialists: ", error);
-    res.status(500).json({ error: "Error creating specialists" });
+    res.status(500).json({ error: "Error creating specialists: " + error });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const specialist = await Specialist.findByPk(req.params.id);
+    res.json(specialist);
+  } catch (error) {
+    console.log("Error getting specialist: ", error);
+    res.status(500).json({ error: "Error getting specialist: " + error });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const specialist = await Specialist.findByPk(req.params.id);
+    if (specialist) {
+      const updatedSpecialist = await specialist.update({ ...specialist, ...req.body });
+      res.json(updatedSpecialist);
+    }
+  } catch (error) {
+    console.log("Error updateing specialist: ", error);
+    res.status(500).json({ error: "Error updating specialist: " + error });
   }
 });
 
