a46977163b75a39701e97ae7a47a472bc1436210
Ben Durfee <ben.p.durfee@gmail.com>
2023-06-07 17:27:37 -0400

    add model tests

--- server/models/user.ts
+++ server/models/user.ts
@@ -9,6 +9,9 @@ class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
   declare name: string;
 
   public static async hashPassword(user: User): Promise<void> {
+    if (user.password.length < 9 || user.password.length > 20 || typeof user.password !== "string") {
+      throw new Error("Validation error: password must be 9 to 20 characters long");
+    }
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(user.password, saltRounds);
     user.password = hashedPassword;
@@ -22,17 +25,36 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    password: {
-      field: "user_password",
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     username: {
       type: DataTypes.STRING,
-      unique: true,
+      unique: {
+        name: "unique",
+        msg: "Validation error: username must be unique",
+      },
       allowNull: false,
+      validate: {
+        len: {
+          args: [5, 30],
+          msg: "username must be between 5 and 30 characters",
+        },
+        noSpaces(username: string) {
+          if (/\s/.test(username)) {
+            throw new Error("Validation error: username cannot contain spaces");
+          }
+        },
+      },
     },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [3, 30],
+          msg: "name must be between 3 and 30 characters",
+        },
+      },
+    },
+    password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
