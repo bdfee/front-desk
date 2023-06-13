4bdd843fecb8db64d2d5a4333d6d754036059419
Ben Durfee <ben.p.durfee@gmail.com>
2023-06-13 10:43:55 -0400

    drop tables route for testing

--- server/services/db.ts
+++ server/services/db.ts
@@ -1,3 +1,12 @@
+import { dropAllTables } from "../tests/helpers/models";
+
 export const isTestDb = () => {
   return process.env.NODE_ENV === "test" || process.env.NODE_ENV === "dev";
+};
+
+export const dropTables = async () => {
+  if (isTestDb()) {
+    await dropAllTables();
+    return true;
+  } else return false;
 };
