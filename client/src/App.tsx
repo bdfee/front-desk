aa3227a57ed75f99c8782c500017215d69dd5d81
Ben Durfee <ben.p.durfee@gmail.com>
2023-06-16 14:17:30 -0400

    add landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
 import SpecialistPage from './components/specialist-page'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { Typography, Container, Divider, Button, Box } from '@mui/material'
 
 const App = () => {
   return (
     <div className="App">
-      <SpecialistPage />
+      <Router>
+        <Container>
+          <Typography variant="h2" style={{ marginBottom: '.05em' }}>
+            frontdesk
+          </Typography>
+          <Button component={Link} to="/" style={{ margin: '0px 5px' }}>
+            front
+          </Button>
+          <Button component={Link} to="/specialists">
+            specialists
+          </Button>
+          <Divider style={{ marginBottom: '1em' }} />
+          <Routes>
+            <Route path="/specialists" element={<SpecialistPage />} />
+          </Routes>
+        </Container>
+      </Router>
     </div>
   )
 }
