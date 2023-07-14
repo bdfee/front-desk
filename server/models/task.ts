307906197c0e58999378982051910466e4fa412e
Ben Durfee <ben.p.durfee@gmail.com>
2023-07-14 08:35:04 -0400

    model

---
+++ server/models/task.ts
@@ -0,0 +1,59 @@
+import { Model, DataTypes, InferAttributes, InferCreationAttributes, CreationOptional, ForeignKey } from "sequelize";
+import { sequelize } from "../utils/connectToDb";
+
+class Task extends Model<InferAttributes<Task>, InferCreationAttributes<Task>> {
+  declare taskId: CreationOptional<number>;
+  declare dueDate: string;
+  declare description: string;
+  declare userId: ForeignKey<number>;
+  declare patientId: ForeignKey<number> | null;
+  declare specialistId?: ForeignKey<number> | null;
+
+  static initModel(): void {
+    Task.init(
+      {
+        taskId: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        dueDate: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            notEmpty: true,
+            isDate: true,
+          },
+        },
+        description: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+          validate: {
+            notEmpty: true,
+          },
+        },
+        userId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        patientId: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+        },
+        specialistId: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        underscored: true,
+        timestamps: false,
+        tableName: "tasks",
+        modelName: "task",
+      }
+    );
+  }
+}
+
+export default Task;
